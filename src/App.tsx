042f064c80b9c246f4990112dc3352295afe5537
lbaud21 <lucas.baudino@outlook.fr>
2023-06-11 21:24:43 +0200

    feat: install rainbowkit + wagmi and setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import FileAccessForm from "./Components/FileAccessForm";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { filecoin } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains(
+  [filecoin],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "HackFS",
+  projectId: import.meta.env.VITE_WALLET_CONNECT,
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 function App() {
   return (
-    <>
-      <FileAccessForm />
-    </>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <FileAccessForm />
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
